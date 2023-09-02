ca292511bcf30e7f2cf65c7d26af63f69510f218
RomanZemlin <roman.zemlin@mail.ru>
2023-09-02 14:08:32 +0300

    add h_w

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+from django.apps import AppConfig
 
-# Register your models here.
+
+class CatalogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'catalog'
