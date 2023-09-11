be3557bd57e26b56184f1b80df440e9f2f4433d6
RomanZemlin <roman.zemlin@mail.ru>
2023-09-11 19:09:18 +0300

    add h_w 20_2

--- catalog/models.py
+++ catalog/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-NULLABLE = {'null': True, 'blank': True}
-
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    description = models.TextField(verbose_name='Описание', null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -18,12 +16,12 @@ class Category(models.Model):
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание', **NULLABLE)
-    image = models.ImageField(verbose_name='Изображение', upload_to='img/', **NULLABLE)
+    description = models.TextField(verbose_name='Описание', null=True, blank=True)
+    image = models.ImageField(verbose_name='Изображение', upload_to='img/', null=True, blank=True)
     category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
-    price = models.IntegerField(verbose_name='Цена', **NULLABLE)
+    price = models.IntegerField(verbose_name='Цена', null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    date_modified = models.DateTimeField(verbose_name='Дата последнего изменения', **NULLABLE)
+    date_modified = models.DateField(auto_now=True, verbose_name='Дата последнего изменения', null=True, blank=True)
 
     def __str__(self):
         return self.name
--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'catalog.apps.CatalogConfig',
+    'catalog',
 
     'django.contrib.admin',
     'django.contrib.auth',
